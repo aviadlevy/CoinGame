9b1556ede50df59bee647d2e3ea9bae0945b0a4b
Aviad Levy <aviadlevy1@gmail.com>
2018-02-15 20:32:47 +0200

    progress

--- app/src/main/java/com/example/cp/cpcoingame/AboutActivity.java
+++ app/src/main/java/com/example/cp/cpcoingame/AboutActivity.java
@@ -1,5 +1,7 @@
 package com.example.cp.cpcoingame;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -8,6 +10,12 @@ import android.view.View;
  */
 
 public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 
     public void goBack(View view) {
         finish();

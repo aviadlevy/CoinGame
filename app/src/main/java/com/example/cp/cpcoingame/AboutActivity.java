0e2dfb2e4b614d03dc20bcf417f789f9e8820ace
Aviad <aviadl>
2018-02-15 16:09:21 +0200

    progress

---
+++ app/src/main/java/com/example/cp/cpcoingame/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.example.cp.cpcoingame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by cp on 2/15/2018.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    public void goBack(View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/example/cp/cpcoingame/MainActivity.java
+++ app/src/main/java/com/example/cp/cpcoingame/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cp.cpcoingame;
 
+import android.content.Intent;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +17,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private final int MOVE_DELTA = 50;
+    private final int PLAYER_X = 350;
+    private final int PLAYER_Y = 350;
+    private final int COIN_X = 50;
+    private final int COIN_Y = 50;
 
     private int score;
     private ImageView coin;
@@ -89,4 +98,49 @@ public class MainActivity extends AppCompatActivity {
         coin.setY(y);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuNewGame:
+                startNewGame();
+                return true;
+            case R.id.menuShowAbout:
+                showAboutScreen();
+                return true;
+            case R.id.menuShowWebPage:
+                showWebPageScreen();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showWebPageScreen() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://google.com/"));
+        startActivity(intent);
+    }
+
+    private void showAboutScreen() {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    private void startNewGame() {
+        player.setX(PLAYER_X);
+        player.setY(PLAYER_Y);
+        coin.setX(COIN_X);
+        coin.setY(COIN_Y);
+        score = -5;
+        updateScore();
+    }
+
+
 }
